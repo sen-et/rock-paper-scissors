f9f016e248794cc7deb95de3f82b86572f8fe88a
sen-et <sen-et@users.noreply.github.com>
2024-02-11 18:22:46 -0800

    Create function for playing a single round against computer

--- main.js
+++ main.js
@@ -21,3 +21,41 @@ function getComputerChoice() {
     };
     return result;
 }
+
+// function playRound(playerSelection, computerSelection) {
+    // store playerSelection in variable that converts to lowercase
+    // call getComputerChoice function and store result in variable
+    // create conditional that compares playerSelection to compSelection
+        // if playerSelection is rock and compSelection is rock return tie
+        // if playerSelection is rock and compSelection is paper return loss
+        // else return win
+        // repeat for all possible permutations
+// }
+
+function playRound(playerSelection, computerSelection) {
+    let player1 = playerSelection.toLowerCase();
+    let player2 = computerSelection.toLowerCase();
+    let result;
+    if (player1 == 'rock' && player2 == 'rock') {
+        result = 'You have tied! Try again.'
+    } else if (player1 == 'rock' && player2 == 'paper') {
+        result = 'You have lost! Try again.'
+    } else if (player1 == 'rock' && player2 == 'scissors') {
+        result = 'You have won! Well done.'
+    } else if (player1 == 'paper' && player2 == 'paper') {
+        result = 'You have tied! Try again.'
+    } else if (player1 == 'paper' && player2 == 'scissors') {
+        result = 'You have lost! Try again.'
+    } else if (player1 == 'paper' && player2 == 'rock') {
+        result = 'You have won! Well done.'
+    } else if (player1 == 'scissors' && player2 == 'scissors') {
+        result = 'You have tied! Try again.'
+    } else if (player1 == 'scissors' && player2 == 'rock') {
+        result = 'You have lost! Try again.'
+    } else {
+        result = 'You have won! Well done.'
+    }
+    return result;
+};
+
+let computerSelection = getComputerChoice();
